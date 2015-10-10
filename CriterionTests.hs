4c4f6ea661f1f77f79060b66b57e0baed82868cf
Jonathan Jeffrey <jonrjeffrey@gmail.com>
2015-10-10 17:26:27 -0400

    Show random matrices to be tested

--- CriterionTests.hs
+++ CriterionTests.hs
@@ -4,6 +4,10 @@ import System.Random
 import Criterion.Main
 import Control.DeepSeq (($!!))
 
+data NamedMatrix a = NMatrix String (Matrix a)
+
+instance (Show a) => Show (NamedMatrix a) where 
+        show (NMatrix string matr) = "\n\n" ++ string ++ "\n\n" ++ show matr
 
 main = do 
 	putStrLn "Enter a number to seed random generator, or type 'get' to use getStdGen."
@@ -14,8 +18,10 @@ main = do
 			else mkStdGen (read response :: Int)
 	putStrLn "Run benchmarks up to what nxn size matrix?"
         size <- getLine
+        let matrixSize = (read size :: Int)
+        putStrLn $ show $ generateSolvedRandomMatrices matrixSize stdGen
         defaultMain [
-			bgroup "hungarianMin" $ generateBenchmarks (read size :: Int) stdGen
+			bgroup "hungarianMin" $ generateBenchmarks matrixSize stdGen
 	            ]
 
 
@@ -30,5 +36,17 @@ nthStdGen x g = if x < 0
 			else nthStdGen (x - 1) $ snd $ next g
 
 generateBenchmarks :: Int -> StdGen -> [Benchmark]
-generateBenchmarks 0 g = []
+generateBenchmarks 0 _ = []
 generateBenchmarks x g = generateBenchmarks (x-1) (snd (next g)) ++ [bench (show x ++ "x" ++ show x) $ (whnf hungarianMin) $!! randTestMatrix g x]
+
+generateSolvedRandomMatrices :: Int -> StdGen -> [NamedMatrix Int]
+generateSolvedRandomMatrices 0 _ = []
+generateSolvedRandomMatrices x g = 
+        let randName = "Random " ++ sizeStr x ++ " Matrix"
+            randMatrix = randTestMatrix g x
+            solvedName = "Solved Random " ++ sizeStr x ++ " Matrix"
+            solvedMatrix = hungarianMin randMatrix
+            in generateSolvedRandomMatrices (x-1) (snd (next g)) ++ NMatrix randName randMatrix : [NMatrix solvedName solvedMatrix]
+
+sizeStr :: Int -> String
+sizeStr x = show x ++ "x" ++ show x
